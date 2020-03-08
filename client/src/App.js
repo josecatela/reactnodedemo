9566b2216942ef63b6fcdac329e82fc3c3140d91
José Catela <josecatela@gmail.com>
2020-03-08 15:29:10 +0000

    part 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/layouts/Home";
+import About from "./components/layouts/About";
+import Layout from "./components/layouts/Layout";
+import NewsArticle from "./components/containers/NewsArticle";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 class App extends Component {
   render() {
-    return <Home />;
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Layout>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/news/:id" component={NewsArticle} />
+          </Layout>
+        </BrowserRouter>
+      </Provider>
+    );
   }
 }
 
--- client/src/components/presentation/NewsItemListing.js
+++ client/src/components/presentation/NewsItemListing.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class NewsItemListing extends Component {
   render() {
     return (
       <div>
         <div>
-          <b>{this.props.title}</b>
+          <div>
+            <Link to={`/news/${this.props.data.id}`}>
+              <b>{this.props.data.title}</b>
+            </Link>
+          </div>
+          <div>{this.props.data.teaser}</div>
         </div>
         <div>{this.props.teaser}</div>
       </div>
@@ -15,9 +21,11 @@ class NewsItemListing extends Component {
 }
 
 NewsItemListing.propTypes = {
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  teaser: PropTypes.string.isRequired
+  data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    teaser: PropTypes.string.isRequired
+  })
 };
 
 export default NewsItemListing;
